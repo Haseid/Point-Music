27a7bf5843649ccd1f95a091cc8c21315ac39e80
Haseid <larshaseid@gmail.com>
2018-01-05 16:37:15 +0100

    rework to class style coding, more comments

--- point_music.py
+++ point_music.py
@@ -1,86 +1,83 @@
+from tkinter import *
 from os import listdir		#get file names
 from pygame import mixer	#package to play mp3 files
-import random				#rng
-import tkinter
-songsDir = "Music/"			#music directory
-songs = listdir(songsDir)	#fetching song-file names
-ant = len(songs)			#amount of songs
-random_numbers = []			#array with random numbers
-history = []				#array to keep track of the songs in the random order
-history_index = -1			#index to keep track of history
-mixer.init()
+from random import sample
 
-def Next_song():
-	global random_numbers
-	global history
-	global history_index
-	history_index += 1
-	while (len(history) <= history_index):
-		if random_numbers == []:
-			random_numbers = random.sample(range(ant), ant)
-		random_number = random_numbers.pop()
-		history.append (random_number)
-	label_3.config(text=songs[history[history_index]][:-4])
+class Gui(object):
+	songs = listdir("Music/")	#fetching song-file names
+	ant = len(songs)			#amount of songs
+	random_numbers = []			#array with random numbers
+	history = []				#array to keep track of the songs in the random order
+	history_index = -1			#index to keep track of history
 
-def Previous_song():
-	global history_index
-	if history_index > 0:
-		history_index -= 1
-		label_3.config(text=songs[history[history_index]][:-4])
+	def __init__(self, master):
+		# setting up master frame
+		master.title("Point Music")
+		master.resizable(width=FALSE, height=FALSE)
 
-def Start_music():
-	mixer.music.load(songsDir+songs[history[history_index]])	#loads a new song
-	mixer.music.play()
-	label_1.config(text=songs[history[history_index]][:-4])
-	button_1.config(text="Fade out", command=Stop_music)
-	Next_song()
+		# creating frames
+		self.left_frame = Frame(master, bg="black", width=400, height=300)
+		self.right_frame = Frame(master, bg="green", width=400, height=300)
+		self.inner_left_frame = Frame(self.left_frame, bg="red")
+		self.inner_right_frame = Frame(self.right_frame, bg="red")
 
-def Stop_music():
-	mixer.music.fadeout(1500)									#fadeout over milliseconds
-	button_1.config(text="Play next", command=Start_music)
+		# layout for frames
+		self.left_frame.grid(row=0, column=0)
+		self.right_frame.grid(row=0, column=1)
+		self.inner_left_frame.place(in_=self.left_frame, anchor="c", relx=.5, rely=.5)
+		self.inner_right_frame.place(in_=self.right_frame, anchor="c", relx=.5, rely=.5)
 
-graphic = tkinter.Tk()
-graphic.title("Point Music")
-frame_main = tkinter.Frame(graphic, width=500, height=150)		#main frame
-frame_center_outer = tkinter.Frame(frame_main)					#centred frame
-frame_center_left = tkinter.Frame(frame_center_outer)
-frame_center_right = tkinter.Frame(frame_center_outer)
-frame_text_upper_1 = tkinter.Frame(frame_center_left)
-frame_buttons_lower_1 = tkinter.Frame(frame_center_left)
-frame_text_upper_2 = tkinter.Frame(frame_center_right)		
-frame_buttons_lower_2 = tkinter.Frame(frame_center_right)
+		# creating widgets in left frame
+		self.label_static_1 = Label(self.inner_left_frame, text="Playing now:")
+		self.label_playing_song = Label(self.inner_left_frame, text="Nothing")
+		self.button_play = Button(self.inner_left_frame, text="Play next", command=self.start_music)
 
-frame_main.pack(fill="both")
-frame_center_outer.place(in_=frame_main, anchor="c", relx=.5, rely=.5)
-frame_center_left.pack(side=tkinter.LEFT, anchor="w")
-frame_center_right.pack(side=tkinter.RIGHT, anchor="e")
-#left side
-frame_text_upper_1.pack(side=tkinter.TOP)
-frame_buttons_lower_1.pack(side=tkinter.BOTTOM)
-#right side
-frame_text_upper_2.pack(side=tkinter.TOP)
-frame_buttons_lower_2.pack(side=tkinter.BOTTOM)
+		# layout for widgets in left frame
+		self.label_static_1.grid(row= 0)
+		self.label_playing_song.grid(row= 1)
+		self.button_play.grid(row= 2)
 
-#left side
-label_0 = tkinter.Label(frame_text_upper_1, text="Playing now:")
-label_1 = tkinter.Label(frame_text_upper_1, text="Nothing")
-button_1 = tkinter.Button(frame_buttons_lower_1, text="Start music", command=Start_music)
-button_2 = tkinter.Button(frame_buttons_lower_1, text="Exit", command=graphic.quit)
-#right side
-label_2 = tkinter.Label(frame_text_upper_2, text="Next song:")
-label_3 = tkinter.Label(frame_text_upper_2, text="")
-button_3 = tkinter.Button(frame_buttons_lower_2, text="Previous", command=Previous_song)
-button_4 = tkinter.Button(frame_buttons_lower_2, text="Next", command=Next_song)
+		# creating widgets in right frame
+		self.label_static_2 = Label(self.inner_right_frame, text="Playing now:")
+		self.label_next_song = Label(self.inner_right_frame, text="")
+		self.button_previous = Button(self.inner_right_frame, text="Previous", command=self.previous_song)
+		self.button_next = Button(self.inner_right_frame, text="Next", command=self.next_song)
 
-label_0.pack(side=tkinter.TOP)
-label_1.pack()
-button_1.pack(side=tkinter.LEFT)
-button_2.pack(side=tkinter.LEFT)
+		# layout for widgets in right frame
+		self.label_static_2.grid(row=0, columnspan=2)
+		self.label_next_song.grid(row=1, columnspan=2)
+		self.button_previous.grid(row=2, column=0)
+		self.button_next.grid(row=2, column=1)
 
-label_2.pack(side=tkinter.TOP)
-label_3.pack()
-button_3.pack(side=tkinter.LEFT)
-button_4.pack(side=tkinter.LEFT)
+		# start up
+		mixer.init()
+		self.next_song()
 
-Next_song()
-graphic.mainloop()
+	def start_music(self):
+		mixer.music.load("Music/"+self.songs[self.history[self.history_index]])
+		mixer.music.play()
+		self.label_playing_song.config(text=self.songs[self.history[self.history_index]][:-4])
+		self.button_play.config(text="Fade out", command=self.stop_music)
+		self.next_song()
+
+	def stop_music(self):
+		mixer.music.fadeout(1500)
+		self.button_play.config(text="Play next", command=self.start_music)
+
+	def next_song(self):
+		self.history_index += 1
+		while (len(self.history) <= self.history_index):
+			if self.random_numbers == []:
+				self.random_numbers = sample(range(self.ant), self.ant)
+			self.random_number = self.random_numbers.pop()
+			self.history.append (self.random_number)
+		self.label_next_song.config(text=self.songs[self.history[self.history_index]][:-4])
+
+	def previous_song(self):
+		if self.history_index > 0:
+			self.history_index -= 1
+			self.label_next_song.config(text=self.songs[self.history[self.history_index]][:-4])
+
+root = Tk()
+window = Gui(root)
+root.mainloop()
